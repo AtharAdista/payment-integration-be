aebf9c34b62cae451c6f5dca763c2d4c3f7786c8
Athar Adista <athar0165@gmail.com>
2025-06-29 17:44:27 +0700

    fix: ignore godotenv if production

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -20,11 +20,13 @@ import (
 
 func main() {
 
-	err := godotenv.Load("../../.env")
-
-	if err != nil {
+	if os.Getenv("ENV") != "production" {
+	err := godotenv.Load("../../.env") 
+		if err != nil {
 		log.Fatal("Error loading .env file")
+		}
 	}
+
 
 	port := os.Getenv("PORT")
 
